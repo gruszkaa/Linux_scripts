019ac881c05928006f4faa4ab565eef8677a587b
gruszkaa <nnykiel.na@gmail.com>
2023-12-14 23:37:05 +0100

    final version

--- script1.sh
+++ script1.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-# Wykonaj catkin_make
-catkin_make
+#Do <cmd_name> <--it's a cmd that is triggering <cmd2_name> in open terminals
+<cmd_name>
 
-# Wykonaj source ./devel/setup.bash w każdym otwartym terminalu
-terminal_list=$(xdotool search --class gnome-terminal)  # Znajdź wszystkie otwarte terminale
+# Do <cmd2_name> in every open Linux's terminal window
+terminal_list=$(xdotool search --class gnome-terminal)  # Looking for opened terminals
 
 for terminal_id in $terminal_list; do
-    xdotool windowactivate --sync $terminal_id  # Aktywuj terminal
-    xdotool type "source ./devel/setup.bash"  # Wprowadź komendę
-    xdotool key Return  # Wciśnij Enter
+    xdotool windowactivate --sync $terminal_id  # Activating terminal
+    xdotool type "<cmd2_name>"  #Type given commend
+    xdotool key Return  # Press enter
 done
+
+#EXAMPLE:
+##!/bin/bash
+#catkin_make
+
+#terminal_list=$(xdotool search --class gnome-terminal) 
+
+#for terminal_id in $terminal_list; do
+#    xdotool windowactivate --sync $terminal_id
+#    xdotool type "source ./devel/setup.bash" 
+#    xdotool key Return  
+#done
